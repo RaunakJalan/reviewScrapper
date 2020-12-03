aaf33b12d28f7ed2e6190f26c5ff2648f923063f
RaunakJalan <ronakjalan98@gmail.com>
2020-12-03 10:20:20 +0530

    removed port definition

--- app.py
+++ app.py
@@ -45,5 +45,5 @@ def index():
         return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(port=8001, debug=True)
-    # app.run(debug=True)
+    # app.run(port=8001, debug=True)
+    app.run(debug=True)
